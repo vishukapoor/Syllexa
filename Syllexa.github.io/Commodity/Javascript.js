f374a847d6acfd565981eede06c093d2c363109c
vishu kapoor <vishu.kapoor@outlook.com>
2015-11-18 23:15:42 +0530

    Bootstraping

--- Syllexa.github.io/Commodity/Javascript.js
+++ Syllexa.github.io/Commodity/Javascript.js
@@ -1,11 +1,9 @@
               // Set the dimensions of the canvas / graph
             // Main is for charts and mini is for slider which would be below the chart
-            var main_margin = {top: 20, right: 80, bottom: 100, left: 40},
-                mini_margin = {top: 430, right: 80, bottom: 20, left: 40},
-                main_width = 960 - main_margin.left - main_margin.right,
-                main_height = 500 - main_margin.top - main_margin.bottom,
-                mini_height = 500 - mini_margin.top - mini_margin.bottom;
-
+          var margin = {top: 20, right: 20, bottom: 30, left: 60},
+              ww = document.getElementById("chart").clientWidth,
+              width = ww - margin.left - margin.right,
+              height = 500 - margin.top - margin.bottom;
             // Parse the date / time
             var formatDate = d3.time.format("%d-%m-%Y"), 
                 parseDate = formatDate.parse,
