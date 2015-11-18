6d19de324aa46e04a4ed7d095ce117d61a329f79
vishu kapoor <vishu.kapoor@outlook.com>
2015-11-18 18:17:09 +0530

    Revert "Removed date from slider"
    This reverts commit 0e9d765c0e817bceca7563305ca5dacba0493450.

--- Syllexa.github.io/Commodity/Javascript.js
+++ Syllexa.github.io/Commodity/Javascript.js
@@ -34,6 +34,10 @@
                 .scale(main_x)
                 .tickFormat(d3.time.format("%d-%m-%Y"))
                 .orient("bottom"),
+                mini_xAxis = d3.svg.axis() //this is for slider
+                .scale(mini_x)
+                .tickFormat(d3.time.format("%d-%m-%Y"))
+                .orient("bottom");
 
             var main_yAxisLeft = d3.svg.axis()
                 .scale(main_y0)
