54f094f5f84ce34ede35892785f3594c06a7abb2
vishu kapoor <vishu.kapoor@outlook.com>
2015-11-19 14:02:06 +0530

    Data Updated

--- Syllexa.github.io/R-Scripts/Currency.R
+++ Syllexa.github.io/R-Scripts/Currency.R
@@ -1,23 +1,21 @@
 Currency<- function(){
-
-library(rjson)
+    
   
-Function<-dget(file = "C:\\Users\\Vishu\\OneDrive\\Data Scraping\\NSE-BSE Historical Prices\\GoogFin.R")
 
-USD_INR<- Function(ticker = "USDINR",exchange = "",TimeFrame = "1Y")
-colnames(USD_INR)[5]<- "USD_INR"
-
-Pound_INR<- Function(ticker = "GBPINR",exchange = "",TimeFrame = "1Y")
-colnames(Pound_INR)[5]<-"Pound_INR"
-
-Euro_INR<-Function(ticker = "EURINR",exchange = "",TimeFrame = "1Y")
-colnames(Euro_INR)[5]<-"Euro_INR"
-
-Currency<- cbind(USD_INR,Pound_INR,Euro_INR)
-
-Currency<- Currency[,c("Date","USD_INR","Pound_INR","Euro_INR")]
-
-Currency_JSON<-toJSON(Currency)
-
-write(Currency_JSON,"C:\\Users\\Vishu\\Documents\\GitHub\\Syllexa\\Chart\\Currency\\Currency.json")
+  Function<-dget(file = "https://raw.githubusercontent.com/vishukapoor/Syllexa/gh-pages/Syllexa.github.io/R-Scripts/GoogFin.R")
+  
+  USD_INR<- Function(ticker = "USDINR",exchange = "",TimeFrame = "1Y")
+  colnames(USD_INR)[5]<- "USD_INR"
+  
+  Pound_INR<- Function(ticker = "GBPINR",exchange = "",TimeFrame = "1Y")
+  colnames(Pound_INR)[5]<-"Pound_INR"
+  
+  Euro_INR<-Function(ticker = "EURINR",exchange = "",TimeFrame = "1Y")
+  colnames(Euro_INR)[5]<-"Euro_INR"
+  
+  Currency<- cbind(USD_INR,Pound_INR,Euro_INR)
+  
+  Currency<- Currency[,c("Date","USD_INR","Pound_INR","Euro_INR")]
+  
+  write.csv(Currency,"C:\\Users\\Vishu\\Documents\\GitHub\\Syllexa\\Syllexa.github.io\\Currency\\Currency.csv")
 }
