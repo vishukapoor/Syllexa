a3cefbfe30d2f9f1fe6bd754f53ffa17ec0a7698
vishu kapoor <vishu.kapoor@outlook.com>
2015-11-27 10:05:21 +0530

    Data Updated

--- Syllexa.github.io/R-Scripts/Commodities.R
+++ Syllexa.github.io/R-Scripts/Commodities.R
@@ -14,7 +14,7 @@ Commodity<- function(){
 
   links <- xpathSApply(doc, "//a/@href")
 
-  Links<-links[grep("2014",links,ignore.case = T)] 
+  Links<-links[grep("2015",links,ignore.case = T)] 
 
   Links<- Links[c(-1,-2)]
 
